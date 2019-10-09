016f99703e49aad5bf4120cf62b046f865a1e25a
Alejandro Lafourcade <alafourcade@quarksoft.net>
2019-10-09 17:04:09 -0300

    Load Balancer

--- src/main/java/py/com/ale/productsservice/controllers/ProductRestController.java
+++ src/main/java/py/com/ale/productsservice/controllers/ProductRestController.java
@@ -1,7 +1,11 @@
 package py.com.ale.productsservice.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +15,30 @@ import py.com.ale.productsservice.models.service.IProductService;
 @RestController
 public class ProductRestController {
 
-  private final IProductService productService;
+    private final IProductService productService;
 
-  public ProductRestController(IProductService productService) {
-    this.productService = productService;
-  }
+    @Autowired
+    private Environment environment;
 
-  @GetMapping("/listar")
-  public List <Product> listar() {
-    return productService.findAll();
-  }
+    @Value("${server.port}")
+    private Integer port;
 
-  @GetMapping("ver/{id}")
-  public Product detalle(@PathVariable Long id) {
-    return productService.findById(id);
-  }
+    public ProductRestController(IProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/listar")
+    public List<Product> listar() {
+        return productService.findAll().stream().map(product -> {
+            product.setPort(port);
+            return product;
+        }).collect(Collectors.toList());
+    }
+
+    @GetMapping("ver/{id}")
+    public Product detalle(@PathVariable Long id) {
+        Product product = productService.findById(id);
+        product.setPort(port);
+        return product;
+    }
 }
